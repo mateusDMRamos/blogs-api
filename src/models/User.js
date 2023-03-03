06920b9663fa9683ef81d093caf3c626d3858193
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-03 16:21:01 -0300

    feat: criado model User

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
