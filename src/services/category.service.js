577ad94930f3a9ff41cc7df5cae252be90a2e7a9
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-06 16:20:50 -0300

    feat: criado método para registrar novas categorias à API

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models/Category');
+const { Category } = require('../models');
 
 const setNewCategory = async (categoryName) => {
   const category = await Category.create({ name: categoryName });
